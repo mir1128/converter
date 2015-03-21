9176e9efe23823b3d9991797ccb7b69d2a9bea6e
jie.liu <jie.liu@rea-group.com>
2015-03-22 01:55:16 +0800

    add log4j.

--- src/main/java/com/looobo/Main.java
+++ src/main/java/com/looobo/Main.java
@@ -1,6 +1,10 @@
 package com.looobo;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
+        
     }
 }
